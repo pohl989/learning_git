6e84c68e6b0a7478f76a0f994af5131913aefe05
pohl989 <pohl989@gmail.com>
2018-08-22 19:49:18 -0600

    committing for safety

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
    
     // variables
+    var player
     var name = document.getElementById('name')
     var startButton = document.getElementById('start-button')
+    var top = document.getElementById('top')
     
     name.addEventListener('keyup', function(event){
         if (event.target.value !== "") {
@@ -13,7 +15,21 @@ document.addEventListener("DOMContentLoaded", function(){
         
     })
 
+
     //functions
+    function startGame() {
+        player = name.value 
+        startButton.classList="hide"
+        var label = document.createElement('p')
+        label.innerHTML = 'Welcome ' + player 
+        label.className = 'center'
+        top.append(label)
+    }
+
+
+
+
+    startButton.addEventListener('click', startGame)
 
 
 
