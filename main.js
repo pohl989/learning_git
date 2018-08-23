084c4ed290deeb1d0978595223d72f2331c43baa
pohl989 <pohl989@gmail.com>
2018-08-22 20:04:57 -0600

    updated some cool code

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", function(){
     var name = document.getElementById('name')
     var startButton = document.getElementById('start-button')
     var top = document.getElementById('top')
+    var gameArea = document.getElementById('game-area')
+    var numGuesses = 0
+    var correctGuesses = 0
+    var ballPosition
+    var userGuess
     
     name.addEventListener('keyup', function(event){
         if (event.target.value !== "") {
@@ -17,6 +22,38 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
     //functions
+    function genBallPosition() {
+        //generate winning ball location
+    }
+
+    function incCounters() {
+        //if correct correct guesses
+        //if wrong guess increment wrong guess
+        //update scoreboard accoring
+    }
+
+    function calcAverage(){
+        // worry about this last
+        // divide winning from attempted guesses
+    }
+
+    function makeGuess(){
+        genBallPosition()
+    }
+
+
+    function drawBoxes() {
+        for(var i = 0; i < 3; i++){
+            var box = document.createElement('div')
+            box.className = 'box'
+            box.id = 'box-' + i
+            gameArea.append(box)
+            // game is stopped, need to make it continue with makeGuess
+            box.addEventListener('click', makeGuess)
+        }
+    }
+
+
     function startGame() {
         player = name.value 
         startButton.classList="hide"
@@ -24,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function(){
         label.innerHTML = 'Welcome ' + player 
         label.className = 'center'
         top.append(label)
+        drawBoxes()
     }
 
 
