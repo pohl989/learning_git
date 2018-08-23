6e9931532087224ec48edf6fb718e6328d9ed6be
pohl989 <pohl989@gmail.com>
2018-08-22 19:29:00 -0600

    Here ya go

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
-    //the entire game is inside here
+   
     // variables
-
+    var name = document.getElementById('name')
+    var startButton = document.getElementById('start-button')
+    
+    name.addEventListener('keyup', function(event){
+        if (event.target.value !== "") {
+            startButton.className=""
+        } else {
+            startButton.className="hide"
+        }
+        
+    })
 
     //functions
 
